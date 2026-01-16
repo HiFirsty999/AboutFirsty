7705d9a72641b86a8fbe3cea0ce06b1914a40bb3
Firsty <fc.firsty@gmail.com>
2026-01-16 17:59:30 +0700

    Add files via upload

---
+++ scripts/mainstyle.js
@@ -0,0 +1,13 @@
+function PointerEnter(ElementName) {
+    var UIElement = document.getElementById(ElementName);
+    
+    UIElement.style.backgroundColor = "white";
+    UIElement.style.color = "black";
+}
+
+function PointerLeave(ElementName) {
+    var UIElement = document.getElementById(ElementName);
+    
+    UIElement.style.backgroundColor = "grey";
+    UIElement.style.color = "white";
+}
